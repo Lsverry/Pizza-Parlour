d3ea6bd93d0a21db328f7c31c3dd7b1e7df3d554
Luis Echeverry <nmx0040@gmail.com>
2024-05-12 09:46:54 +0000

    readies code for deploy

--- pizzaParlour/settings.py
+++ pizzaParlour/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9$k#sv(3w#%-&a0yddi61#w&m5hbacd2tihops14wc(*+bck0u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'menu',
 ]
 
 MIDDLEWARE = [
